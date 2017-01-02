1d41e4471b37b9c6abd1a9b825f165c45596d499
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-01-02 19:13:28 +1100

    Fixed repo

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,7 +2,7 @@ class openhab::config {
   vcsrepo { '/opt/openhab/conf':
     ensure   => present,
     provider => 'git',
-    source   => 'https://github.com/dylanratcliffe/openhab.git',
+    source   => 'https://github.com/dylanratcliffe/openhab_config.git',
     revision => 'master',
     owner    => 'openhab',
     group    => 'openhab',
